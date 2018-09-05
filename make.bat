17b1b2804a659c360e718751a3d769e0dcf894fe
Jannes Timm <jayco@gmx.de>
2018-09-05 17:58:50 +0200

    add icon, change build scripts - icon not showing on mac

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-pipenv run pyinstaller --onefile --windowed --add-data jieba/;jieba --add-data src/resources/;resources --noconfirm --hidden-import PyQt5.sip src/qtmain.py
+pipenv run pyinstaller --onefile --windowed --add-data jieba/;jieba --add-data src/resources/;resources -icon=icon.ico --noconfirm --hidden-import PyQt5.sip src/qtmain.py
--- make.sh
+++ make.sh
@@ -2,5 +2,7 @@ rm -rf dist/qtmain dist/qtmain.app
 pipenv run pyinstaller --onefile --windowed \
     --add-data jieba/:jieba \
     --add-data src/resources/:resources --noconfirm \
+    --name Hanzi-extract \
+    --icon icon.icns \
     --hidden-import PyQt5.sip src/qtmain.py 
-cp -f Info.plist dist/qtmain.app/Contents/Info.plist
+cp -f Info2.plist dist/Hanzi-extract.app/Contents/Info.plist
